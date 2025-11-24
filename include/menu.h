6152e1f580d00608aae4265c3ff805e3d70de503
FalconPeek <lucaslindstrom808@gmail.com>
2025-11-23 23:23:37 -0300

    modificado el MENU

--- include/menu.h
+++ include/menu.h
@@ -27,7 +27,7 @@ void switchMenu(){
             menuMedicos(0);
         break;
         case 6:
-            printf("\n\nCerrando programa...\n\n");
+            cprintf(COL_BRIGHT_CYAN, "\n\nCerrando programa...\n\n");
             Sleep(1500);
             exit(EXIT_SUCCESS);
         break;
@@ -42,29 +42,29 @@ void cargarDatosBase(){
         {
         case 0:
         clearScreen();
-            printf("\nCargando programa.\n");
+            cprintf(COL_BRIGHT_CYAN, "\nCargando programa.\n");
             Sleep(1000);
             break;
         case 1:
         clearScreen();
-            printf("\nCargando programa..\n");
+            cprintf(COL_BRIGHT_CYAN, "\nCargando programa..\n");
             Sleep(1000);
             break;
         case 2:
         clearScreen();
-            printf("\nCargando programa...\n\nCargando bases de datos.");
+            cprintf(COL_BRIGHT_CYAN, "\nCargando programa...\n\nCargando bases de datos.");
             Sleep(1000);
             
             break;
         case 3:
             clearScreen();
-            printf("\nCargando bases de datos..\n");
+            cprintf(COL_BRIGHT_CYAN, "\nCargando bases de datos..\n");
             Sleep(1000);
             
             break;
         case 4:
             clearScreen();
-            printf("\nCargando bases de datos...\n");
+            cprintf(COL_BRIGHT_CYAN, "\nCargando bases de datos...\n");
             cantidadDeMedicos = cargarMedicosEnArray(arrayDeMedicos, MAXMEDICOS, true);
             Sleep(1000);
             break;
@@ -77,10 +77,10 @@ void cargarDatosBase(){
             Sleep(1000);
             agregarPacientes(&colaPacientes, arrayPacientes, cantidadDePacientesTotalGral, true);
             Sleep(500);
-            if(cantidadDePacientesTotalGral == cantidadPacientesEnCola(colaPacientes)) printf("\nTotalidad de pacientes agregados correctamente\n");
+            if(cantidadDePacientesTotalGral == cantidadPacientesEnCola(colaPacientes)) cprintf(COL_BRIGHT_CYAN, "\nTotalidad de pacientes agregados correctamente\n");
         break;
         case 7:
-            printf("\nPrograma ya cargado y compilado... Iniciando...\n\n");
+            cprintf(COL_BRIGHT_CYAN, "\nPrograma ya cargado y compilado... Iniciando...\n\n");
             Sleep(1000);    
         break;
         }
@@ -114,12 +114,12 @@ void menu(){
     cprintf(COL_GREEN, "\t================Menu Principal===============\n");
     cprintf(COL_GREEN, "\t=============================================\n");
 
-    printf("\n[1]"); cprintf(COL_BLUE, " - Agregar paciente");
-    printf("\n[2]"); cprintf(COL_BLUE, " - Ver cola de Pacientes");
-    printf("\n[3]"); cprintf(COL_BLUE, " - Ver lista de pacientes por DNI y Nombre");
-    printf("\n[4]"); cprintf(COL_BLUE, " - Mostrar registro de pacientes");
-    printf("\n[5]"); cprintf(COL_BLUE, " - Abrir menu de medicos (privado)");
-    printf("\n[6]"); cprintf(COL_BLUE, " - Cerra programa");
+    cprintf(COL_BRIGHT_CYAN, "\n[1]"); cprintf(COL_RESET, " - Agregar paciente");
+    cprintf(COL_BRIGHT_CYAN, "\n[2]"); cprintf(COL_RESET, " - Ver cola de Pacientes");
+    cprintf(COL_BRIGHT_CYAN, "\n[3]"); cprintf(COL_RESET, " - Ver lista de pacientes por DNI y Nombre");
+    cprintf(COL_BRIGHT_CYAN, "\n[4]"); cprintf(COL_RESET, " - Mostrar registro de pacientes");
+    cprintf(COL_BRIGHT_CYAN, "\n[5]"); cprintf(COL_RESET, " - Abrir menu de medicos (privado)");
+    cprintf(COL_BRIGHT_CYAN, "\n[6]"); cprintf(COL_RESET, " - Cerra programa");
     cprintf(COL_BRIGHT_BLUE, "\n\nOpcion: ");
 
 
